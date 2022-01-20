2b73e13cddbae51bc9b8264cfdcf735dbdbc64ed
Juan David Kincaid <juandavidkincaid@gmail.com>
2022-01-20 13:22:53 -0500

    added hardhat for flattening sources

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.11",
+  networks: {
+  },
+};
+
+export default config;
