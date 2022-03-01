6457aec5b3fc83daaa2c79846a9fb3b123a8097c
Juan David Suarez <juandavidkincaid@gmail.com>
2022-02-28 23:41:21 -0500

    Added dev contract

---
+++ scripts/deployDev.ts
@@ -0,0 +1,38 @@
+import { ethers, network } from "hardhat";
+
+async function main() {
+  const accounts = await ethers.getSigners();
+
+  if (network.name === "localhost") {
+    await ethers.provider.send("hardhat_setBalance", [
+      accounts[0].address,
+      ethers.utils.parseUnits("1000", "ether").toHexString(),
+    ]);
+  }
+
+  const latticeEnvVarName = `${network.name.toLocaleUpperCase()}_LATTICE_TOKEN`;
+  const latticeTokenAddress = process.env[latticeEnvVarName];
+  if (!latticeTokenAddress) {
+    throw new Error(`Unable to find env variable ${latticeEnvVarName}`);
+  }
+
+  console.log(`Network Name: ${network.name}`);
+  console.log(`Lattice Token Address: ${latticeTokenAddress}`);
+  console.log(`Signer Address: ${accounts[0].address}`);
+
+  const LatticeStakingPoolFactory = await ethers.getContractFactory(
+    "LatticeStakingPoolDev"
+  );
+  const contract = await LatticeStakingPoolFactory.deploy(latticeTokenAddress);
+
+  console.log(`Waiting for transaction: ${contract.deployTransaction.hash}`);
+
+  await contract.deployed();
+
+  console.log("LatticeStakingPoolDev deployed to:", contract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
